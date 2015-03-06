323eb3ae0a330d0eb0636f4dc29dcb0d5e8dfab8
Joseph P Geringer <joegeringer@gmail.com>
2015-03-06 16:30:15 -0600

    Lines are now css!
    Lines are now completely css driven, also added a font.

--- backup/index.php
+++ backup/index.php
@@ -21,7 +21,7 @@
 			<li ng-repeat="x in games | orderBy: 'game' " ng-click="showControls(x.game)">
 				<a href="#" data-game="{{x.game}}">
 					<img ng-src="{{x.iconpath}}" />
-					<!--{{error}}-->
+					{{error}}
 				</a>
 			</li>
 		</ol>
@@ -38,46 +38,57 @@
 
 			<div ng-class="{active: button.buttonp1coin}" class="button-p1-coin">
 				<span>{{button.buttonp1coin}}</span>
+				<img src="/assets/img/controls/button-p1-coin.png" />
 			</div>
 
 			<div ng-class="{active: button.buttonp1start}" class="button-p1-start">
 				<span>{{button.buttonp1start}}</span>
+				<img src="/assets/img/controls/button-p1-start.png" />
 			</div>
 
 			<div ng-class="{active: button.buttonadmin1}" class="button-admin1">
 				<span>{{button.buttonadmin1}}</span>
+				<img src="/assets/img/controls/button-admin1.png" />
 			</div>
 
 			<div ng-class="{active: button.buttonadmin2}" class="button-admin2">
 				<span>{{button.buttonadmin2}}</span>
+				<img src="/assets/img/controls/button-admin2.png" />
 			</div>
 
 			<div ng-class="{active: button.buttonadmin3}" class="button-admin3">
 				<span>{{button.buttonadmin3}}</span>
+				<img src="/assets/img/controls/button-admin3.png" />
 			</div>
 
 			<div ng-class="{active: button.button1}" class="button-1">
 				<span>{{button.button1}}</span>
+				<img src="/assets/img/controls/button-1.png" />
 			</div>
 
 			<div ng-class="{active: button.button2}" class="button-2">
 				<span>{{button.button2}}</span>
+				<img src="/assets/img/controls/button-2.png" />
 			</div>
 
 			<div ng-class="{active: button.button3}" class="button-3">
 				<span>{{button.button3}}</span>
+				<img src="/assets/img/controls/button-3.png" />
 			</div>
 
 			<div ng-class="{active: button.button4}" class="button-4">
 				<span>{{button.button4}}</span>
+				<img src="/assets/img/controls/button-4.png" />
 			</div>
 
 			<div ng-class="{active: button.button5}" ng-class="{active: button.button1}" class="button-5">
 				<span>{{button.button5}}</span>
+				<img src="/assets/img/controls/button-5.png" />
 			</div>
 
 			<div ng-class="{active: button.button6}" class="button-6">
 				<span>{{button.button6}}</span>
+				<img src="/assets/img/controls/button-6.png" />
 			</div>
 
 		</div>
--- head.php
+++ head.php
@@ -2,12 +2,13 @@
 <meta name="apple-mobile-web-app-capable" content="yes">
 <meta name="mobile-web-app-capable" content="yes">
 <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+<link href='http://fonts.googleapis.com/css?family=Roboto+Condensed' rel='stylesheet' type='text/css'>
 <link rel="apple-touch-icon-precomposed" sizes="152x152" href="apple-touch-icon-precomposed.png">
 <link rel="apple-touch-icon" href="apple-touch-icon-precomposed.png">
 <link rel="icon" href="favicon.png">
 <link type="text/css" rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/normalize/3.0.2/normalize.min.css" />
 <link type="text/css" rel="stylesheet" href="/assets/css/font-awesome.css" />
-<link type="text/css" rel="stylesheet" href="/assets/css/main.css" />
+<link type="text/css" rel="stylesheet" href="/assets/css/arcade.css" />
 <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.28/angular.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.13/angular-route.min.js"></script>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
